d20459eebd738f391df838955e9cb95127396849
mrpatches123 <dwcox42@tntech.edu>
2026-04-20 21:28:01 -0500

    better parsing

--- src/LED/led_test.c
+++ src/LED/led_test.c
@@ -1,22 +1,51 @@
 #include "./led.h"
+#include <errno.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+	char *end = NULL;
+	long blinkCount;
+	long onMs;
+	long offMs;
 
-	printf("Number of arguments: %d\n", argc);
-	if (argc != 3)
+	if (argc != 4)
+	{
+		fprintf(stderr, "Usage: %s <count> <on_ms> <off_ms>\n", argv[0]);
 		return 1;
-	long blinkCount = strtol(argv[1]);
-	long onMs = strtol(argv[2]);
-	long offMs = strtol(argv[3]);
+	}
+
+	errno = 0;
+	blinkCount = strtol(argv[1], &end, 10);
+	if (errno != 0 || end == argv[1] || *end != '\0' || blinkCount <= 0 || blinkCount > 65535)
+	{
+		fprintf(stderr, "Invalid count: %s\n", argv[1]);
+		return 1;
+	}
+
+	errno = 0;
+	onMs = strtol(argv[2], &end, 10);
+	if (errno != 0 || end == argv[2] || *end != '\0' || onMs <= 0 || onMs > 65535)
+	{
+		fprintf(stderr, "Invalid on_ms: %s\n", argv[2]);
+		return 1;
+	}
+
+	errno = 0;
+	offMs = strtol(argv[3], &end, 10);
+	if (errno != 0 || end == argv[3] || *end != '\0' || offMs <= 0 || offMs > 65535)
+	{
+		fprintf(stderr, "Invalid off_ms: %s\n", argv[3]);
+		return 1;
+	}
 
 	Led *led = led_create(17);
 	if (!led)
 		return 1;
 
-	if (led_blink(led, blinkCount, onMs, offMs, 0) < 0)
+	if (led_blink(led, (uint16_t)blinkCount, (uint16_t)onMs, (uint16_t)offMs, 0) < 0)
 	{
 		led_destroy(led);
 		return 1;
