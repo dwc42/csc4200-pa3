39385b0fb0d0703bcdcbfbd2f4d36ddf70e2f3cb
mrpatches123 <dwcox42@tntech.edu>
2026-04-20 21:26:46 -0500

    bad test arg parsing should work for now

--- src/LED/led_test.c
+++ src/LED/led_test.c
@@ -1,13 +1,22 @@
 #include "./led.h"
 #include <unistd.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+
+	printf("Number of arguments: %d\n", argc);
+	if (argc != 3)
+		return 1;
+	long blinkCount = strtol(argv[1]);
+	long onMs = strtol(argv[2]);
+	long offMs = strtol(argv[3]);
+
 	Led *led = led_create(17);
 	if (!led)
 		return 1;
 
-	if (led_blink(led, 10, 100, 400, 0) < 0)
+	if (led_blink(led, blinkCount, onMs, offMs, 0) < 0)
 	{
 		led_destroy(led);
 		return 1;
