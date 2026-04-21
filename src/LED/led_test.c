e831ce742c66a472bcfe02a7725c2eb4667f05bf
mrpatches123 <dwcox42@tntech.edu>
2026-04-20 21:15:23 -0500

    added led life time to fix it posibly not working

--- src/LED/led_test.c
+++ src/LED/led_test.c
@@ -1,7 +1,21 @@
 #include "./led.h"
+#include <unistd.h>
 
 int main()
 {
 	Led *led = led_create(17);
-	led_blink(led, 10, 1000, 1000, 0);
+	if (!led)
+		return 1;
+
+	if (led_blink(led, 10, 1000, 1000, 0) < 0)
+	{
+		led_destroy(led);
+		return 1;
+	}
+
+	while (led_busy(led))
+		usleep(10000);
+
+	led_destroy(led);
+	return 0;
 }
